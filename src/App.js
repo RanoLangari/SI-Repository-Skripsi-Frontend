0074c066adbe57cb6f78d16709fdbe4c4604c4a9
Rano Langari <langarirano@gmail.com>
2024-01-09 20:17:56 +0800

    -

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import DetailSkripsi from "./components/mahasiswa/detailSkripsi";
 import ProfileMahasiswa from "./components/mahasiswa/profileMahasiswa";
 import AdminDashboard from "./components/admin/dashboard";
 import DataSkripsi from "./components/admin/dataSkripsi";
+import ProfileAdmin from "./components/admin/profileAdmin";
 import "./index.css";
 
 function App() {
@@ -26,6 +27,7 @@ function App() {
           <Route path="/mhs/detail-skripsi/:id" element={<DetailSkripsi />} />
           <Route path="/admin/dashboard" element={<AdminDashboard />} />
           <Route path="/admin/data-skripsi" element={<DataSkripsi />} />
+          <Route path="/admin/profile" element={<ProfileAdmin />} />
         </Routes>
       </BrowserRouter>
     </div>
