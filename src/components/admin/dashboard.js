68d11b89e2e8c4105f7229798e983a0afd1388a5
Rano Langari <langarirano@gmail.com>
2024-01-12 23:30:39 +0800

    -

--- src/components/admin/dashboard.js
+++ src/components/admin/dashboard.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Swal from "sweetalert2";
-import MahasiswaTable from "./mahasiswaTable";
 import { useMemo } from "react";
 import {
   MaterialReactTable,
