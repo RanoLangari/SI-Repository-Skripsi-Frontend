034717dff6f765b0a5ee9c008f90dec0343e1de7
Rano Langari <langarirano@gmail.com>
2024-01-18 00:24:11 +0800

    -

--- src/components/mahasiswa/auth/register-mhs.js
+++ src/components/mahasiswa/auth/register-mhs.js
@@ -219,7 +219,7 @@ const RegisterMahasiswa = () => {
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="submit"
             >
-              Daftar Akun
+              Register
             </button>
             <a
               className="inline-block align-baseline font-normal text-sm text-blue-500 hover:text-blue-800"
