9f4c0be4a95af05c8aa7a282e0ab05fbb1ab3b08
Rano Langari <langarirano@gmail.com>
2024-01-28 23:27:57 +0800

    refactor

--- src/components/mahasiswa/auth/register-mhs.js
+++ src/components/mahasiswa/auth/register-mhs.js
@@ -54,184 +54,157 @@ const RegisterMahasiswa = () => {
     }
   };
   return (
-    <div className="flex items-center justify-center bg-gray-200">
-      <div className="w-full max-w-md ml-4 mr-4">
-        <div className="flex justify-center mb-4">
-          <img src="./FEB.png" alt="" className="mx-auto mb-4" />
-        </div>
+    <div className="flex items-center justify-center h-screen bg-gray-200">
+      <div className="flex items-center justify-center bg-gray-200">
+        <div className="w-full max-w-md ml-4 mr-4">
+          <div className="flex justify-center mb-4">
+            <img src="./FEB.png" alt="" className="mx-auto mb-4" />
+          </div>
 
-        <form
-          className="bg-white shadow-lg rounded px-12 pt-6 pb-8 mb-4"
-          onSubmit={handleRegister}
-        >
-          <div
-            className="text-gray-800 text-2xl flex justify-center border-b-2 py-2 mb-4"
-            style={{ fontFamily: "Roboto, sans-serif" }}
+          <form
+            className="bg-white shadow-lg rounded px-12 pt-6 pb-8 mb-4"
+            onSubmit={handleRegister}
           >
-            Register Mahasiswa
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-normal mb-2"
-              htmlFor="nim"
+            <div
+              className="text-gray-800 text-2xl flex justify-center  py-2 mb-8"
+              style={{ fontFamily: "Roboto, sans-serif" }}
             >
-              <b>NIM</b>
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              name="nim"
-              id="nim"
-              type="text"
-              placeholder="nim"
-              required
-              onChange={(e) => {
-                setNim(e.target.value);
-              }}
-            />
-          </div>
-          {/* nama */}
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-normal mb-2"
-              htmlFor="nama"
-            >
-              <b>Nama</b>
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              name="nama"
-              id="nama"
-              type="text"
-              placeholder="nama"
-              required
-              onChange={(e) => {
-                setNama(e.target.value);
-              }}
-            />
-          </div>
-          {/* jurusan */}
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-normal mb-2"
-              htmlFor="jurusan"
-            >
-              <b>Jurusan</b>
-            </label>
-            <select
-              name="jurusan"
-              id="jurusan"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              required
-              onChange={(e) => {
-                setJurusan(e.target.value);
-              }}
-            >
-              <option value={""}>-- Pilih Jurusan --</option>
-              <option value={"Manajemen"}>Manajemen</option>
-              <option value={"Akuntansi"}>Akuntansi</option>
-              <option value={"Ekonomi Pembangunan"}>Ekonomi Pembangunan</option>
-            </select>
-          </div>
-          {/* semester */}
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-normal mb-2"
-              htmlFor="semester"
-            >
-              <b>Semester</b>
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              name="semester"
-              id="semester"
-              type="text"
-              placeholder="semester"
-              required
-              onChange={(e) => {
-                setSemester(e.target.value);
-              }}
-            />
-          </div>
-          {/* status kelulusan */}
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-normal mb-2"
-              htmlFor="status_kelulusan"
-            >
-              <b>Status Kelulusan</b>
-            </label>
-            <select
-              name="status_kelulusan"
-              id="status_kelulusan"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              required
-              onChange={(e) => {
-                setStatusKelulusan(e.target.value);
-              }}
-            >
-              <option value={""}>-- Pilih Status Kelulusan --</option>
-              <option value={"Lulus"}>Lulus</option>
-              <option value={"Belum Lulus"}>Belum Lulus</option>
-            </select>
-          </div>
-          <div className="mb-6">
-            <label
-              className="block text-gray-700 text-sm font-normal mb-2"
-              htmlFor="password"
-            >
-              <b>Password</b>
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              name="password"
-              id="password"
-              type="password"
-              placeholder="***"
-              required
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-            />
-          </div>
-          {/* confirm-password */}
-          <div className="mb-6">
-            <label
-              className="block text-gray-700 text-sm font-normal mb-2"
-              htmlFor="confirm-password"
-            >
-              <b>Confirm Password</b>
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              name="confirm-password"
-              id="confirm-password"
-              type="password"
-              placeholder="***"
-              required
-              onChange={(e) => {
-                setConfirmPassword(e.target.value);
-              }}
-            />
-          </div>
+              Register Mahasiswa
+            </div>
+            <div className="flex flex-col md:flex-row">
+              <div className="flex flex-col w-full md:w-1/2 md:pr-2">
+                <label className="text-gray-700 dark:text-gray-200 text-left">
+                  NIM
+                </label>
+                <input
+                  type="text"
+                  className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+                  onChange={(e) => setNim(e.target.value)}
+                />
+              </div>
+              <div className="flex flex-col w-full md:w-1/2 md:pl-2">
+                <label className="text-gray-700 dark:text-gray-200 text-left">
+                  Jurusan
+                </label>
+                <select
+                  name="jurusan"
+                  id="jurusan"
+                  className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+                  required
+                  onChange={(e) => {
+                    setJurusan(e.target.value);
+                  }}
+                >
+                  <option value={""}>-- Pilih Jurusan --</option>
+                  <option value={"Manajemen"}>Manajemen</option>
+                  <option value={"Akuntansi"}>Akuntansi</option>
+                  <option value={"Ekonomi Pembangunan"}>
+                    Ekonomi Pembangunan
+                  </option>
+                </select>
+              </div>
+            </div>
 
-          <div className="flex items-center justify-between">
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="submit"
-            >
-              Register
-            </button>
-            <a
-              className="inline-block align-baseline font-normal text-sm text-blue-500 hover:text-blue-800"
-              href="login-mhs"
-            >
-              Login Mahasiswa
-            </a>
-          </div>
-        </form>
-        <p className="text-center text-gray-500 text-xs">
-          &copy;2023 FEB UNDANA. All rights reserved.
-        </p>
+            <div className="flex flex-col md:flex-row">
+              <div className="flex flex-col w-full md:w-1/2 md:pr-2">
+                <label className="text-gray-700 dark:text-gray-200 text-left">
+                  Nama
+                </label>
+                <input
+                  type="text"
+                  className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+                  onChange={(e) => setNama(e.target.value)}
+                />
+              </div>
+              <div className="flex flex-col w-full md:w-1/2 md:pl-2">
+                <label className="text-gray-700 dark:text-gray-200 text-left">
+                  Semester
+                </label>
+                <select
+                  name="semester"
+                  id="semester"
+                  className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+                  required
+                  onChange={(e) => {
+                    setSemester(e.target.value);
+                  }}
+                >
+                  <option value={""}>-- Pilih Semester --</option>
+                  {[...Array(14)].map((_, index) => (
+                    <option key={index + 1} value={(index + 1).toString()}>
+                      {index + 1}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+
+            <div className="flex flex-col md:flex-row">
+              <div className="flex flex-col w-full md:w-1/2 md:pr-2">
+                <label className="text-gray-700 dark:text-gray-200 text-left">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div className="flex flex-col w-full md:w-1/2 md:pl-2">
+                <label className="text-gray-700 dark:text-gray-200 text-left">
+                  Konfirmasi Password
+                </label>
+                <input
+                  type="password"
+                  className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+                  value={confirm_password}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+              </div>
+            </div>
+
+            {/* status kelulusan */}
+            <div className="flex flex-col md:flex-row">
+              <div className="flex flex-col w-full md:w-1/2 md:pr-2">
+                <label className="text-gray-700 dark:text-gray-200 text-left">
+                  Status Kelulusan
+                </label>
+                <select
+                  name="status_kelulusan"
+                  id="status_kelulusan"
+                  className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+                  required
+                  onChange={(e) => {
+                    setStatusKelulusan(e.target.value);
+                  }}
+                >
+                  <option value={""}>-- Pilih Status Kelulusan --</option>
+                  <option value={"Lulus"}>Lulus</option>
+                  <option value={"Tidak Lulus"}>Tidak Lulus</option>
+                </select>
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between mt-8">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="submit"
+              >
+                Register
+              </button>
+              <a
+                className="inline-block align-baseline font-normal text-sm text-blue-500 hover:text-blue-800"
+                href="login-mhs"
+              >
+                Login Mahasiswa
+              </a>
+            </div>
+          </form>
+          <p className="text-center text-gray-500 text-xs">
+            &copy;2023 FEB UNDANA. All rights reserved.
+          </p>
+        </div>
       </div>
     </div>
   );
