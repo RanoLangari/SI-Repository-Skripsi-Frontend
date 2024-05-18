8b65a54089bccad4d366c3a4a0fb4a87d3f8c574
Rano Langari <langarirano@gmail.com>
2024-05-18 23:07:14 +0800

    -

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,24 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig(() => {
   return {
     resolve: {
-        alias: {
-          '~': path.resolve(__dirname, './src'),
-        },
+      alias: {
+        "~": path.resolve(__dirname, "./src"),
       },
+    },
     server: {
-      port: 8080,
-      // open: true,
+      host: "0.0.0.0",
+      port: 3000,
+    },
+    preview: {
+      host: "0.0.0.0",
+      port: 8000,
     },
     build: {
-      outDir: 'build',
+      outDir: "build",
     },
     plugins: [react()],
   };
